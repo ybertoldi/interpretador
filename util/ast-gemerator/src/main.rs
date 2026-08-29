f63ab19bbdbdf2634a6da39fd896e1fbe3703cf5
ybertoldi <yuri.bertoldi003@gmail.com>
2026-08-29 18:27:05 -0300

    começando gerador de código para AST

---
+++ util/ast-gemerator/src/main.rs
@@ -0,0 +1,108 @@
+use std::collections::HashSet;
+
+static AST_VALS: &[&str] = &[
+    "Binary: Expr left, Token operator, Expr right",
+    "Grouping: Expr expression",
+    "Literal: Object value",
+    "Unary: Token operator, Expr right",
+];
+
+#[derive(Debug, Clone)]
+struct Field {
+    fd_name: String,
+    fd_type: String,
+}
+#[derive(Debug, Clone)]
+struct Class {
+    name: String,
+    fields: Vec<Field>,
+}
+fn main() {
+    let (classes, _class_names) = parse_classes();
+
+    generate_structs(&classes);
+    generate_enum(&classes);
+    generate_new(&classes);
+}
+
+fn parse_classes() -> (Vec<Class>, HashSet<String>) {
+    let mut classes = Vec::new();
+    let mut class_names = HashSet::new();
+    for v in AST_VALS {
+        let split: Vec<_> = v.split(":").map(|s| s.trim()).collect();
+        let [cls_nm, fields] = split.as_slice() else {
+            panic!("could not parse classname and fields!");
+        };
+
+        let parsed_fields = fields
+            .split(",")
+            .map(|s| s.split_whitespace().collect::<Vec<_>>())
+            .map(|splt| {
+                let [fd_type, fd_name] = splt.as_slice() else {
+                    panic!("could not parse field {split:?}")
+                };
+                Field {
+                    fd_name: fd_name.to_string(),
+                    fd_type: fd_type.to_string(),
+                }
+            })
+            .collect();
+
+        classes.push(Class {
+            name: cls_nm.to_string(),
+            fields: parsed_fields,
+        });
+        class_names.insert(cls_nm.to_string());
+    }
+
+    (classes, class_names)
+}
+
+fn generate_structs(classes: &Vec<Class>) {
+    // struct definitions
+    for c in classes {
+        println!("pub struct {} {{", c.name);
+        for f in &c.fields {
+            if f.fd_type == "Expr" {
+                println!("    {}: Box<{}>,", f.fd_name, f.fd_type);
+            } else {
+                println!("    {}: {},", f.fd_name, f.fd_type);
+            }
+        }
+        println!("}}\n");
+    }
+}
+
+fn generate_enum(classes: &Vec<Class>) {
+    println!("pub enum Expr {{");
+    for c in classes {
+        println!("    {}({}),", c.name, c.name);
+    }
+    println!("}}\n");
+}
+
+fn generate_new(classes: &Vec<Class>) {
+    for c in classes {
+        println!("impl {} {{", c.name);
+
+        // function head
+        print!("    pub fn new(");
+        for f in &c.fields {
+            print!("{}: {},", f.fd_name, f.fd_type);
+        }
+        println!(") -> Self {{");
+
+        // function body
+        println!("        Self{{");
+        for f in &c.fields {
+            if f.fd_type == "Expr" {
+                println!("            {}: Box::new({}),", f.fd_name, f.fd_name);
+            } else {
+                println!("            {},", f.fd_name);
+            }
+        }
+        println!("        }}");
+        println!("    }}");
+        println!("}}\n");
+    }
+}
